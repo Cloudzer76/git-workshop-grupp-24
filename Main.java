79892fe90a1bf3ac25ad50f1be78bf732b64d652
Rasmus <rasmus.strandh@hotmail.com>
2025-03-28 09:11:52 +0100

    Changed Main, Updated main

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Tjena");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Skriv in nåt kul");
+        String fun = input.nextLine();
+        System.out.println(fun);
+        System.out.println("Det var inte så kul");
+
     }
 }
