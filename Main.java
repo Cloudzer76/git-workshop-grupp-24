926b1165a6f03320831bfc27af42254999515853
Daniel Yasini <Danielyasini16@gmail.com>
2025-03-28 10:00:00 +0100

    Daniel main

--- Main.java
+++ Main.java
@@ -6,6 +6,21 @@ public class Main {
         String input;
         System.out.println("Skriv något");
         input = Scanner.nextLine();
-        System.out.println(input);
+
+        if(input.equalsIgnoreCase("Daniel drink")) {
+            food();
+        }
+        else if (input.equalsIgnoreCase("Daniel")) {
+            hello();
+        }
+        else {
+            System.out.println(input);
+        }
+    }
+    public static void food() {
+        System.out.println("Daniel gillar pizza");
+    }
+    public static void hello() {
+        System.out.println("Daniel säger hello");
     }
 }
