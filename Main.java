693cc46a941b0d8712f2f20d1720600550dd92e3
Daniel Yasini <Danielyasini16@gmail.com>
2025-03-28 09:26:15 +0100

    Input läsare

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        scanner Scanner = new Scanner(system.in);
+        String input;
+        System.out.println("Skriv något");
+        input = Scanner.nextLine();
+        System.out.println(input);
+    }
+}
