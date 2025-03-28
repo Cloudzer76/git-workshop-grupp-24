8afb87482b6355bf301b6375cbf1238207db061d
Daniel Yasini <Danielyasini16@gmail.com>
2025-03-28 09:33:03 +0100

    Fil som skriver ut mitt namn

---
+++ daniel.java
@@ -0,0 +1,8 @@
+public class daniel {
+    public static void main(String[] args) {
+        daniel();
+    }
+    public static void daniel() {
+        System.out.println("Hi, I'm Daniel");
+    }
+}
